6cdc1c5cfaa9da1c57d204d6dbd3f7b4297b2229
Ian Gow <iandgow@gmail.com>
2024-08-24 16:09:33 +1000

    Added ravenpack code.

---
+++ rpa/update_rpa.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from db2pq import wrds_update_pq
+import datetime
+
+now = datetime.datetime.now()
+avail_years = range(2000, now.year+1)
+
+wrds_update_pq("rpa_entity_mapping", "ravenpack_common")
+
+def update_equities(year):
+    updated = wrds_update_pq("rpa_full_equities_" + str(year), "ravenpack_full",
+                             col_types = {"timestamp_utc": "timestamptz",
+                                          "rpa_time_utc": "text",
+                                          "event_start_date_utc": "timestamptz", 
+                                          "event_end_date_utc": "timestamptz", 
+                                          "reporting_start_date_utc": "timestamptz",
+                                          "reporting_end_date_utc": "timestamptz"},
+                             sas_schema="rpa")
+    return updated
+
+updated = [ update_equities(year) for year in avail_years]
