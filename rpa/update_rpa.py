930991506e0e77cdb5795f9d52b7cfc86cfdc33c
Ian Gow <iandgow@gmail.com>
2024-08-25 09:19:24 +1000

    Use PostgreSQL schemas.

--- rpa/update_rpa.py
+++ rpa/update_rpa.py
@@ -5,10 +5,10 @@ import datetime
 now = datetime.datetime.now()
 avail_years = range(2000, now.year+1)
 
-wrds_update_pq("rpa_entity_mappings", "rpa")
+wrds_update_pq("rpa_entity_mappings", "ravenpack_common", sas_schema="rpa")
 
 def update_equities(year):
-    updated = wrds_update_pq("rpa_djpr_equities_" + str(year), "rpa",
+    updated = wrds_update_pq("rpa_djpr_equities_" + str(year), "ravenpack_dj",
                              col_types = {"timestamp_utc": "timestamptz",
                                           "rpa_time_utc": "text",
                                           "event_start_date_utc": "timestamptz", 
