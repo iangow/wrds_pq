32ce3e6f3c10b9a752530a064233b0f23b4da99e
Ian Gow <iandgow@gmail.com>
2024-10-01 08:51:23 +1000

    Added files to IBES.

--- ibes/update_ibes.py
+++ ibes/update_ibes.py
@@ -5,3 +5,9 @@ wrds_update_pq("statsum_epsus", "ibes",
 col_types = {'acttims_act': 'string', 
              'anntims_act': 'string'})
 wrds_update_pq("statsumu_epsus", "ibes")
+
+wrds_update_pq("act_xepsus", "ibes", 
+               col_types = {"acttims": "string", "anntims": "string"})
+wrds_update_pq("act_epsus", "ibes",
+               col_types = {"acttims": "string", "anntims": "string"})
+wrds_update_pq("actpsum_epsus", "ibes")
