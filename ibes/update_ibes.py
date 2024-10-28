9dd804276f98db53131076708d21f86bb33e3c75
Ian Gow <iandgow@gmail.com>
2024-10-28 15:46:16 -0400

    Added ibes.idsum.

--- ibes/update_ibes.py
+++ ibes/update_ibes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from db2pq import wrds_update_pq 
 
+wrds_update_pq("idsum", "ibes")
 wrds_update_pq("statsum_epsus", "ibes",
 col_types = {'acttims_act': 'string', 
              'anntims_act': 'string'})
